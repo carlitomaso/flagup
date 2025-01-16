f00bfac5c582fa61209045d1782d8ae92dd1a977
carlitomaso <carlatsopk44@gmail.com>
2025-01-17 04:13:14 +0800

    Implement reroute to home when playerdetails is not present

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,18 +1,29 @@
-import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import FlagUpView from "../components/FlagUpView";
 import PlayerView from "../components/PlayerView";
 
 const UserProfile = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if location.state is not defined
+    if (!location.state) {
+      navigate("/"); // Redirect to the homepage if no state exists
+    }
+  }, [location.state, navigate]); // Add location.state and navigate as dependencies
+
+  // If location.state does not exist, prevent rendering to avoid errors
+  if (!location.state) {
+    return null; // Or a loading spinner/placeholder
+  }
 
   const playerDetails = location.state?.playerDetails;
+
   return (
     <>
-      {playerDetails["privilege"] === "FlagUp" ? (
-        <FlagUpView />
-      ) : (
-        <PlayerView />
-      )}
+      {playerDetails.privilege === "FlagUp" ? <FlagUpView /> : <PlayerView />}
     </>
   );
 };
